3a425e1e6691b62387a1b286093fe0388f917cd7
Amirmahdiial <147088052+Amirmahdiial@users.noreply.github.com>
2024-08-23 02:30:29 +0330

    End

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-const slides =document.querySelectorAll(".slides img");
+const slides =document.querySelectorAll(".slide");
 let slideIndex=0;
 let intervalId =null;
 
-document.addEventListener("DOMContentLoaded" , initializeSlider);
-// initializeSlider();
-function initializeSlider() {
+
+main();
+function main() {
     
-    // if(slides.length > 0){
+    if(slides.length > 0){
     slides[slideIndex].classList.add("displaySlide");
-        // intervalId = setInterval(nextSlide ,5000);
-    // }
+        intervalId = setInterval(nextSlide ,5000);
+    }
 }
-// function showSlide(index){
+console.log(slides);
+function showSlide(index){
 
-//     if (index >= slides.length) {
-//         slideIndex = 0;
-//     }else if (index < 0) {
-//         slideIndex = slides.length - 1;
-//     }
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
 
-//     slides.forEach(slide => {
-//         slide.classList.remove("displaySlide");
-//     });
-//     slides[slideIndex].classList.add("displaySlide");
-// }
-
-
-// function pervSlide(){
-//     clearInterval(intervalId);
-//     slideIndex--;
-//     showSlide(slideIndex);
-// }
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
 
 
-// function nextSlide(){
-//     slideIndex++;
-//     showSlide(slideIndex);
-// }
+function pervSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
